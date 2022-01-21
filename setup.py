bab3a6a3f6516eea803a4f85b16b3e25f48ea05a
DeschampsJ <72499926+DeschampsJ@users.noreply.github.com>
2022-01-21 19:57:39 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -73,35 +73,25 @@ def configBordure(id, mdp, ip, connected, pos, nbCoeur, nbPE):
 
     tn.write(b"route-map Outbound-Peering permit 10\n")
     tn.write(b"match community Outbound-Peering\n")
-    tn.write(b"!\n")
     tn.write(b"route-map Outbound-Peer permit 10\n")
     tn.write(b"match community Outbound-Peer\n")
-    tn.write(b"!\n")
     tn.write(b"route-map Outbound-Customer permit 10\n")
     tn.write(b"match community Outbound-Customer\n")
-    tn.write(b"!\n")
     tn.write(b"route-map Inbound-Peering permit 10\n")
     tn.write(b"set local-preference 50\n")
     tn.write(b"set community 100:50\n")
-    tn.write(b"!\n")
     tn.write(b"route-map Inbound-Peer permit 10\n")
     tn.write(b"set local-preference 100\n")
     tn.write(b"set community 100:100\n")
-    tn.write(b"!\n")
     tn.write(b"route-map Inbound-Customer permit 10\n")
     tn.write(b"set local-preference 150\n")
     tn.write(b"set community 100:150\n")
-
-    tn.write(b"ip community-list expanded Outbound-Customer permit 100:150\n")
-    tn.write(b"ip community-list expanded Outbound-Customer permit 100:100\n")
+    tn.write(b"exit\n")
     tn.write(b"ip community-list expanded Outbound-Customer permit 100:50\n")
-    tn.write(b"!\n")
+    tn.write(b"ip community-list expanded Outbound-Customer permit 100:100\n")
+    tn.write(b"ip community-list expanded Outbound-Customer permit 100:150\n")
     tn.write(b"ip community-list expanded Outbound-Peering permit 100:150\n")
-    tn.write(b"!\n")
     tn.write(b"ip community-list expanded Outbound-Peer permit 100:150\n")
-    tn.write(b"!\n")
-
-    tn.write(b"\n")
     tn.write(b"end\n")
     tn.write(b"exit\n")
     print(tn.read_all().decode("ascii"))
@@ -128,11 +118,13 @@ def configCoeur(id, mdp, ip, nbCoeur, pos, nbPE, incr):
         #tn.write(b"ip ospf 100 area 0\n")
         tn.write(b"no shutdown\n")
         incr+=1
+        tn.write(b"exit\n")
 
     #ospf
     tn.write(b"router ospf 100\n")
     tn.write(b"router-id "+a+b"."+a+b"."+a+b"."+a+b"\n") # @loopback pour ospf
     tn.write(b"mpls ldp autoconfig\n")
+    tn.write(b"exit\n")
 
     #bgp
     tn.write(b"router bgp 100\n")
@@ -161,14 +153,13 @@ def configClient(id, mdp, ip, connected, type, posInConnectedListPE):
 
     tn.write(b"enable\n")
     tn.write(b"cisco\n")
-
     tn.write(b"conf t\n")
-
     tn.write(b"interface GigabitEthernet2/0\n")
     tn.write(b"ip address 192.168."+str(10*(posInConnectedListPE+1)+20*connected[1]).encode("ascii")+ b".2 255.255.255.0\n")
+    tn.write(b"no shutdown\n")
+    tn.write(b"exit\n")
 
-
-    #bgp
+    # bgp
     a = str(100+10*(connected[1]+1)).encode("ascii")
     tn.write(b"router bgp 100\n") # @ loopback du routeur de bordure auquel il est connecté, connected donne [id, pos] du routeur de bordure
     #tn.write(b"network 10.0.0.0 mask 255.255.255.0\n") #on en a besoin pour ping d'un endroit different et verif que tout marche
@@ -182,6 +173,7 @@ def configClient(id, mdp, ip, connected, type, posInConnectedListPE):
         print("Problème type client\n")
 
     tn.write(b"neighbor 192.168."+str(10*(posInConnectedListPE+1)+20*connected[1]).encode("ascii")+ b".1 remote-as "+str(a).encode("ascii")+b"\n") # SELON TYPE CLIENT
+
     tn.write(b"\n")
     tn.write(b"end\n")
     tn.write(b"exit\n")
